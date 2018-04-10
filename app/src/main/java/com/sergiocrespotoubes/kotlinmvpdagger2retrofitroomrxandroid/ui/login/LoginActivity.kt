b9928974b8118afa499e5cbcc0575ba31e1c66f6
Sergi <sergiocrespotoubes@gmail.com>
2018-04-10 20:47:27 +0200

    Removing google api and resources bug fix

--- app/src/main/java/com/sergiocrespotoubes/kotlinmvpdagger2retrofitroomrxandroid/ui/login/LoginActivity.kt
+++ app/src/main/java/com/sergiocrespotoubes/kotlinmvpdagger2retrofitroomrxandroid/ui/login/LoginActivity.kt
@@ -8,10 +8,6 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.provider
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.jakewharton.rxbinding2.view.RxView
 import com.sergiocrespotoubes.kotlinmvpdagger2retrofitroomrxandroid.R
 import com.sergiocrespotoubes.kotlinmvpdagger2retrofitroomrxandroid.components.Firebase
@@ -45,7 +41,6 @@ class LoginActivity : BaseActivity(), LoginContract.View {
         super.onCreate(savedInstanceState)
 
         RxView.clicks(button_sign_in).subscribe({presenter.onGoogleSignInClick()})
-        RxView.clicks(button_google_sign_in).subscribe({presenter.onGoogleSignInClick()})
     }
 
     override fun close() = finish()
@@ -59,32 +54,4 @@ class LoginActivity : BaseActivity(), LoginContract.View {
         firebase.signIn()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode === RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-        }
-    }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-
-            // Signed in successfully, show authenticated UI.
-            //updateUI(account)
-            Log.w("TEST", "signInResult: Success")
-            toast("SUCCESS")
-        } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("TEST", "signInResult:failed code=" + e.statusCode)
-            //updateUI(null)
-            toast("ERROR")
-        }
-
-    }
 }
